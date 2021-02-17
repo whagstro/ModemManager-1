82637e2450d3861ffb893f1365f403e0c66cb3dd
Aleksander Morgado <aleksander@aleksander.es>
2021-02-17 15:21:49 +0100

    libmm-glib,bearer-properties: consume 'rm-protocol' setting

--- libmm-glib/mm-bearer-properties.c
+++ libmm-glib/mm-bearer-properties.c
@@ -608,7 +608,13 @@ mm_bearer_properties_consume_variant (MMBearerProperties *properties,
         mm_bearer_properties_set_allow_roaming (
             properties,
             g_variant_get_boolean (value));
-    else {
+    else if (g_str_equal (key, PROPERTY_RM_PROTOCOL))
+        mm_bearer_properties_set_rm_protocol (
+            properties,
+            g_variant_get_uint32 (value));
+    else if (g_str_equal (key, DEPRECATED_PROPERTY_NUMBER)) {
+        /* NO-OP */
+    } else {
         /* Set error */
         g_set_error (error,
                      MM_CORE_ERROR,
